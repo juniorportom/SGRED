d25994617a6a1455c2f23c8a39811d02e2425d60
jm-gutierrez <jm.gutierrez@uniandes.edu.co>
2018-10-30 22:28:46 -0500

    Paso 1: Se crea test navegacion barra lateral de hu25 (ROJO).
    [skip ci]

---
+++ QueVideo/sgrd_25_tests_cases.py
@@ -0,0 +1,31 @@
+__author__ = 'johanna gutierres - nestor romero'
+
+from unittest import TestCase
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+class Sgrd25FunctionalTest(TestCase):
+
+    def setUp(self):
+        # self.browser = webdriver.Chrome('E:\\Nestor\\OneDrive\\Documentos\\uniandes\\201820\\procesos agiles\\chromedriver.exe')
+        self.browser = webdriver.Chrome('E:\\chromedriver_win32\\chromedriver.exe')
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_crudo_list_link_location(self):
+        self.browser.get('http://localhost:8000')
+        self.browser.implicitly_wait(3)
+        botonMenu = self.browser.find_element_by_id('sidebar-collapse-btn')
+        botonMenu.click()
+
+        span = WebDriverWait(self.browser, 5).until(
+            EC.visibility_of_element_located((By.ID, "produccionSpan")))
+        span.click()
+
+        span2 = WebDriverWait(self.browser, 5).until(
+            EC.visibility_of_element_located((By.XPATH, '//span[text()="Listado de Crudos"]')))
+        self.assertIn('Listado de Crudos', span2.text)

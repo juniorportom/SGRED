88af481a1969ebe894a199d29b04e76a91a955e9
Danny Hurtado <dannylhurtado@gmail.com>
2018-11-02 21:44:13 -0500

    Paso1: set up ambiente de pruebas (ROJO)

--- QueVideo/tests.py
+++ QueVideo/tests.py
@@ -1,6 +1,27 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from _future_ import unicode_literals
 
 from django.test import TestCase
+_author_ = 'Joan Torres - Andres Ortiz - Daniel Hurtado'
 
-# Create your tests here.
+from unittest import TestCase
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Sgrd105FunctionalTest(TestCase):
+
+# Mac OSX installation of selenium
+# brew install selenium-server-standalone
+# brew cask install chromedriver
+# Also
+# https://www.seleniumhq.org/download/
+
+    def setUp(self):
+        chromedriver = '/usr/local/bin/chromedriver'
+        self.browser = webdriver.Chrome(chromedriver)
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()

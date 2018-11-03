ec23f6418d0181fe9594b54bf5f8bad4f44bc85f
Hanner Andrés Ortiz Ramirez <ha.ortiz11@uniandes.edu.co>
2018-11-02 21:52:48 -0500

    Paso2: creacion de test de validacion de link de marca de completado (ROJO)

--- QueVideo/tests.py
+++ QueVideo/tests.py
@@ -1,6 +1,7 @@
 from _future_ import unicode_literals
 
 from django.test import TestCase
+
 _author_ = 'Joan Torres - Andres Ortiz - Daniel Hurtado'
 
 from unittest import TestCase
@@ -12,16 +13,29 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class Sgrd105FunctionalTest(TestCase):
 
-# Mac OSX installation of selenium
-# brew install selenium-server-standalone
-# brew cask install chromedriver
-# Also
-# https://www.seleniumhq.org/download/
+    # Mac OSX installation of selenium
+    # brew install selenium-server-standalone
+    # brew cask install chromedriver
+    # Also
+    # https://www.seleniumhq.org/download/
 
     def setUp(self):
-        chromedriver = '/usr/local/bin/chromedriver'
+        chromedriver = 'D:\\chromedriver_win32\\chromedriver.exe'
         self.browser = webdriver.Chrome(chromedriver)
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
+
+
+    def test_activity_check_link_location(self):
+        self.browser.get('http://localhost:8000/actividades/')
+        self.browser.implicitly_wait(3)
+        botonMenu = self.browser.find_element_by_id('checkAct')
+        botonMenu.click()
+
+        span = WebDriverWait(self.browser, 5).until(
+            EC.visibility_of_element_located((By.ID, "activitySpan")))
+        span.click()
+
+        self.assertIn('Done', span.text)
